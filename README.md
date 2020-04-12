41c28c265ebeacf9a8344f0aee3bd24296f1b7fb
Bianca Rosa <me@biancarosa.com.br>
2020-04-12 13:02:17 -0300

    add a readme and go back to version 0.1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# azdevops-jinja2-templates
+
+This is extension to render jinja2 templates on Azure DevOps, inspired by the [Replace Tokens Task](https://marketplace.visualstudio.com/items?itemName=qetza.replacetokens).
--- task/index.ts
+++ task/index.ts
@@ -1,20 +1,24 @@
 import tl = require("azure-pipelines-task-lib/task");
-import trm = require("azure-pipelines-task-lib/toolrunner");
 
 async function run() {
   try {
-    console.log("EXECUTING COMMANDS");
+    console.log("Setting up vars");
+    await tl.getVariables().forEach(variable => {
+      process.env[variable.name] = variable.value;
+    });
 
+    console.log("Install envtpl");
     await tl
       .tool(tl.which("pip"))
       .arg(["install", "envtpl"])
       .exec();
+    console.log("Envtpl Installed");
 
     await tl
-      .tool(tl.which("bash"))
-      .arg(["envtpl", tl.getInput("file", true) || ""])
+      .tool(tl.which("envtpl"))
+      .arg([tl.getInput("file", true) || ""])
       .exec();
-    console.log("rendered");
+    console.log("Rendered template");
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
