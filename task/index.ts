a0aece626911f8a6cbcc77e0e8cc3d1c56328e5f
Bianca Rosa <me@biancarosa.com.br>
2020-04-21 16:59:53 -0300

    add tests - closes #2

--- task/index.ts
+++ task/index.ts
@@ -2,22 +2,17 @@ import tl = require("azure-pipelines-task-lib/task");
 
 async function run() {
   try {
+    let file = tl.getInput("file", true) || "";
     console.log("Setting up vars");
-    await tl.getVariables().forEach(variable => {
+    await tl.getVariables().forEach((variable) => {
       process.env[variable.name] = variable.value;
     });
 
     console.log("Install envtpl");
-    await tl
-      .tool(tl.which("pip"))
-      .arg(["install", "envtpl"])
-      .exec();
+    await tl.tool(tl.which("pip")).arg(["install", "envtpl"]).exec();
     console.log("Envtpl Installed");
 
-    await tl
-      .tool(tl.which("envtpl"))
-      .arg([tl.getInput("file", true) || ""])
-      .exec();
+    await tl.tool(tl.which("envtpl")).arg([file]).exec();
     console.log("Rendered template");
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message);
