bd2f7e3f994f68b796922b5ffe69c1da7350bc17
Bianca Rosa <me@biancarosa.com.br>
2020-04-23 23:02:23 -0300

    add allow missing as parameter

---
+++ task/tests/allowMissing.ts
@@ -0,0 +1,29 @@
+import ma = require("azure-pipelines-task-lib/mock-answer");
+import tmrm = require("azure-pipelines-task-lib/mock-run");
+import path = require("path");
+
+let taskPath = path.join(__dirname, "..", "index.js");
+let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tmr.setInput("file", "file.tmpl");
+tmr.setInput("allowMissing", "true");
+
+let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
+  which: {
+    pip: "/usr/local/bin/pip",
+    envtpl: "/usr/local/bin/envtpl",
+  },
+  exec: {
+    "/usr/local/bin/pip install envtpl --allow-missing": {
+      code: 0,
+      stdout: "",
+    },
+    "/usr/local/bin/envtpl file.tmpl": {
+      code: 0,
+      stdout: "",
+    },
+  },
+};
+
+tmr.setAnswers(a);
+tmr.run();
