102b44e89b186efb0e33c18f83777ccdfc9d71b4
Bianca Rosa <me@biancarosa.com.br>
2020-05-09 10:40:44 -0300

    allow replace in dir

---
+++ task/tests/successWithDir.ts
@@ -0,0 +1,31 @@
+import ma = require("azure-pipelines-task-lib/mock-answer");
+import tmrm = require("azure-pipelines-task-lib/mock-run");
+import path = require("path");
+
+let taskPath = path.join(__dirname, "..", "index.js");
+let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tmr.setInput("dir", "testDir");
+let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
+  which: {
+    pip: "/usr/local/bin/pip",
+    envtpl: "/usr/local/bin/envtpl",
+  },
+  exec: {
+    "/usr/local/bin/pip install envtpl": {
+      code: 0,
+      stdout: "",
+    },
+    "/usr/local/bin/envtpl file1.tpl": {
+      code: 0,
+      stdout: "",
+    },
+    "/usr/local/bin/envtpl file2.tpl": {
+      code: 0,
+      stdout: "",
+    },
+  },
+};
+
+tmr.setAnswers(a);
+tmr.run();
